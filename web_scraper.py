49c14692534ef09e1be6ca644b993bea4dc8caf9
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-07 20:29:54 -0300

    Create web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# web scraper simples para achar todas a licitações
+def WebScrap():
+    html = urlopen('http://licitacoes.sanepar.com.br/SLI2A000.aspx')
+    bs_sanepar = BeautifulSoup(html, 'html.parser')
+
+    lista_links = []
+
+    for link in bs_sanepar.find_all('a'):
+
+        # Varendo todos os links da pagina
+        if 'href' in link.attrs:
+            linkagora = link.attrs['href']
+
+            # Separando apenas os links de licitações
+            if 'SLI2A100' in linkagora:
+                lista_links.append('http://licitacoes.sanepar.com.br/' + linkagora)
+    return lista_links
