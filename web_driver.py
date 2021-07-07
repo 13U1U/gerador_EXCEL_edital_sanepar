115518aada0959ac5ff45b8cab4e27864ee18697
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-07 20:29:15 -0300

    Create web_driver.py

---
+++ web_driver.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from time import sleep
+
+def WebDriv(lista_links):
+
+    navegador = webdriver.Chrome()
+
+    cont = 0
+
+    for editais in lista_links:
+        cont += 1
+        try:
+            navegador.get(editais)
+            navegador.find_element_by_xpath('//*[@id="wrdb_anexos_0"]').click()
+            navegador.find_element_by_xpath('//*[@id="lbdownload"]').click()
+            navegador.find_element_by_xpath('//*[@id="wbtn_semcadastro"]').click()
+            sleep(1)
+        except:
+            try:
+                navegador.get(lista_links[cont])
+                navegador.find_element_by_xpath('//*[@id="wrdb_anexos_0"]').click()
+                navegador.find_element_by_xpath('//*[@id="lbdownload"]').click()
+                navegador.find_element_by_xpath('//*[@id="wbtn_semcadastro"]').click()
+                sleep(1)
+            except:
+                navegador.get(lista_links[cont])
+                navegador.find_element_by_xpath('//*[@id="wrdb_anexos_1"]').click()
+                navegador.find_element_by_xpath('//*[@id="lbdownload"]').click()
+                navegador.find_element_by_xpath('//*[@id="wbtn_semcadastro"]').click()
+
+    return
