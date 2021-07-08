bea9d8429d063d02eddb5c62dee56395359133ca
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-08 11:53:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ from web_scraper import WebScrap
 # from web_driver import WebDriv
 from PDF_to_string import PdfToString
 from N_licitacao import  NLicitacao
+from cabecalho import  CabecalhoTrado
+from objeto import Objeto
+from data import DataLic
 
 from time import sleep
 import xlsxwriter
 
 
-#Automação Web Para Fazer o Dowlond de todos os editais
+# Automação Web Para Fazer o Dowlond de todos os editais
 LinksProcurar = WebScrap()
 # WebDriv(LinksProcurar)
+
 
 file = xlsxwriter.Workbook("Sanepar_Editais.xlsm")
 excel = file.add_worksheet()
 
+
+# adicionando ao excel
 for numero_edital in range(len(LinksProcurar)):
     numero_edital += 1
 
     TextoStr = PdfToString(numero_edital)
+    cabecalho = CabecalhoTrado(TextoStr)
 
-    excel.write('A'+str(numero_edital), NLicitacao(TextoStr))
+    excel.write('A'+str(numero_edital), NLicitacao(TextoStr)) #numero da licitação
+    excel.write('B'+str(numero_edital), Objeto(TextoStr)) # Objeto
+    excel.write('C'+str(numero_edital), DataLic(cabecalho)) # Data entrega
+
     sleep(0.2)
 
 
