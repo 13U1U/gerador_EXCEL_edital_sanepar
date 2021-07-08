58b46e25c32fb241def94658fd8405fec9fda29e
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-08 12:18:12 -0300

    Create regime.py

---
+++ regime.py
@@ -0,0 +1,18 @@
+import copy
+
+
+def Regime(cabecalho):
+    try:
+        regime_fim = ''
+
+        regime = copy.copy(cabecalho)
+        regimelis = str(regime[-1]).split()
+
+        regime_fim = regime_fim + str(regimelis[-3])+ ' ' + str(regimelis[-2]) + ' ' + str(regimelis[-1])
+
+
+        return regime_fim
+    except:
+        print('erro no regime')
+        return '---'
+      
