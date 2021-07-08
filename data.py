802a9ba6144eafeb22aa3edcd15eaed3b86a1902
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-08 11:54:28 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+import copy
+
+def DataLic(cabecalho):
+    try:
+        data = copy.copy(cabecalho)
+        data_fim = ''
+        lista_data = ''
+        for frases in data:
+            if 'para conhecimento dos interessados' in frases:
+                lista_data = frases.split()
+
+        cortar = lista_data.index('realizar')
+        del lista_data[:cortar]
+
+        for palavra in range(len(lista_data)):
+            data_fim = data_fim + ' ' + lista_data[palavra]
+
+        return data_fim
+    except:
+        return '---'
