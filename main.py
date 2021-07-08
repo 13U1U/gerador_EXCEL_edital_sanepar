51b37f7c9c42768f14df94a16e2286171fa54c20
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-08 01:47:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from web_scraper import WebScrap
-from web_driver import WebDriv
+# from web_driver import WebDriv
+from PDF_to_string import PdfToString
+from N_licitacao import  NLicitacao
+
+from time import sleep
+import xlsxwriter
+
 
 #Automação Web Para Fazer o Dowlond de todos os editais
 LinksProcurar = WebScrap()
-WebDriv(LinksProcurar)
+# WebDriv(LinksProcurar)
+
+file = xlsxwriter.Workbook("Sanepar_Editais.xlsm")
+excel = file.add_worksheet()
+
+for numero_edital in range(len(LinksProcurar)):
+    numero_edital += 1
+
+    TextoStr = PdfToString(numero_edital)
+
+    excel.write('A'+str(numero_edital), NLicitacao(TextoStr))
+    sleep(0.2)
+
+
+file.close()
