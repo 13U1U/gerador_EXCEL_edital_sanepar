c75c2e87c541b0c877409372bb5201bd25d25a1d
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-14 12:30:12 -0300

    Update txt_dados.py

--- txt_dados.py
+++ txt_dados.py
@@ -1,14 +1,20 @@
 def TxtAnalise(lista):
-
     lista_avaliar = []
+    lista_antiga = []
 
-    with open('links_licitacao_sanepar.txt','r+') as arquivo:
+    with open('links_licitacao_sanepar.txt', 'r+') as arquivo:
+        for valor in arquivo:
+            lista_antiga.append(valor.replace('\n',''))
+
 
         for links in lista:
-            if links not in 'links_licitacao_sanepar.txt':
 
-                arquivo.write(str(links)+'\n')
-                lista_avaliar = lista_avaliar.append(links)
+            if str(links) in lista_antiga:
+                pass
+
+            else:
+                arquivo.write(str(links) + '\n')
+                lista_avaliar.append(links)
 
 
     return lista_avaliar
