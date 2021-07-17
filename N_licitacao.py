5fe5d8977e09a0a6ff2ea6b8f3ade72ab47172c2
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-16 23:25:13 -0300

    Update N_licitacao.py

--- N_licitacao.py
+++ N_licitacao.py
@@ -1,18 +1,20 @@
 import copy
 
-
 def NLicitacao(texto):
     try:
         text_n = copy.copy(texto)
         LPdf = text_n.split()
-    
+
         valorN = LPdf.index('N°')
         del LPdf[:valorN+1]
-    
+
         num_licitacao =  LPdf[0]
-    
+
+        if num_licitacao == '1':
+            num_licitacao = LPdf[1]
+
         return num_licitacao
-    
+
     except:
         print('erro no numero da licitação')
         return '---'
