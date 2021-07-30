7cda59b49c5968f77dfc2551a9987f4e8e4bfec2
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-30 17:23:33 -0300

    Update regime.py

--- regime.py
+++ regime.py
@@ -7,12 +7,10 @@ def Regime(cabecalho):
 
         regime = copy.copy(cabecalho)
         regimelis = str(regime[-1]).split()
-
-        regime_fim = regime_fim + str(regimelis[-3])+ ' ' + str(regimelis[-2]) + ' ' + str(regimelis[-1])
-
+        regime_fim = regime_fim + str(regimelis[-3]) + ' ' + str(regimelis[-2]) + ' ' + str(regimelis[-1])
 
         return regime_fim
+
     except:
         print('erro no regime')
         return '---'
-      
